2bc46867881b9a40a63ea8a740534a2b4f97f4cc
gabrieljmatias@gmail.com <gabrieljmatias@gmail.com>
2015-06-17 21:06:04 -0300

    added class Fornecedores

---
+++ MerceariaSistema/src/merceariasistema/Fornecedores.java
@@ -0,0 +1,14 @@
+package merceariasistema;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author UDESC
+ */
+public class Fornecedores {
+    
+}
